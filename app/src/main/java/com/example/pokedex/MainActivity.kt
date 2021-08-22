d3ccb5c115f401fd8b89289c451bcd82a442a554
Ruben Yebran <RubenYebran@gmail.com>
2021-08-22 19:01:59 -0400

    Firebase Analytics

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
